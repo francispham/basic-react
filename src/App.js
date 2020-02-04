887e243e4b4f2bb23bb2c08aee4ef372bef7ce60
Francis <francispham89@gmail.com>
2020-02-04 00:25:33 -0800

    Implemented propTypes & defaultProps

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Movie from './Movie';
 const movies = [
   {
     id: 1,
-    title: 'Star Wars'
+    title: 'Star Wars',
+    desc: 'A space Movie'
   }, {
     id: 2,
     title: 'Spider Main'
@@ -35,7 +36,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}        
+        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}        
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+    // Rules: Every Prop should have a propType, & '.isRequired or defaultProps' should be included for Checking Type Errors
+    static propTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            desc: PropTypes.string
+        })
+    }
+
+    static defaultProps = {
+        desc: 'Description Not Available'
+    }
+
     render() {
         return (
             <div>
-                {this.props.movie.title}
+                <h3>{this.props.movie.title}</h3>
+                <p>{this.props.desc}</p>
             </div>
-        )
+        );
     }
 }
