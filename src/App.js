98d240fd666998650c91a85f04827d7fcccd441e
Francis <francispham89@gmail.com>
2020-02-03 18:57:38 -0800

    Used Inline Conditionals, Implemented State Machine & Setup Event Handler

--- src/App.js
+++ src/App.js
@@ -2,26 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Welcome text="Welcome to Using Props" variable="More Variables" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text="Welcome to Using Props" variable="More Variables" />
+          {
+            this.state.toggle && 
+            <p>This should show or hide</p>
+          }
+          <button onClick={this.toggle}>Show/Hide</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 class Welcome extends Component {
