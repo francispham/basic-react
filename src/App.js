e08a6ae5f98e784b33e8353ea899d6badbf1cd03
Francis <francispham89@gmail.com>
2020-02-03 20:49:34 -0800

    Used Refs to Access DOM Elements

--- src/App.js
+++ src/App.js
@@ -3,32 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    console.log("Constructor");
-  }
-
-  componentWillMount() {
-    // For Setting State without Triggering a Second Render (render() hasn't called yet!)
-    console.log("Will Mount");
-  }
-
-  componentDidMount() {
-    // Interactions is now existed and will be available for Further Actions. 
-    // Will Trigger a Re-render. 
-    console.log("Mounted");
-
-  }
-
-  state = {
-    toggle: true //Happen when called render()
-  }
-
-  toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    })
+  submit = () => {
+    console.log(this.text.value)
   }
 
   render() {
@@ -39,13 +15,14 @@ class App extends Component {
           <Welcome 
             text="Welcome to Using Props" 
             variable="More Variables" 
-            toggle={this.state.toggle}
           />
-          {
-            this.state.toggle && 
-            <p>This should show or hide</p>
-          }
-          <button onClick={this.toggle}>Show/Hide</button>
+          <input type="text" ref={(input) => this.text = input } />
+                                {/* Same as: 
+                                  function(input) {
+                                    this.text = input
+                                  } 
+                                */}
+          <button onClick={this.submit}>Show Value</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -62,8 +39,8 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
-    const { text, variable, toggle } = this.props;
-    console.log(toggle);
+    const { text, variable } = this.props;
+
     return (
       <h1 className="App-title">{text} with {variable}</h1>
     )
