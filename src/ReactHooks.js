3d10f6563367b7e47ed5684a24f1abb7e329b81a
Francis <francispham89@gmail.com>
2020-03-03 15:50:35 -0800

    Implemented useState API

--- src/ReactHooks.js
+++ src/ReactHooks.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ReactHooks = () => (
-    <div className="main-wrapper">
-        <h1>Introduce React Hooks</h1>
-    </div>
-);
+const ReactHooks = () => {
+    // const [value, setValue] = useState(initialState);
+    const [name, setName] = useState('');
+
+    return (
+        <div className="main-wrapper">
+            <h1>Introduce React Hooks</h1>
+            <h3>{name}</h3>
+            <input type="text" onChange={e => setName(e.target.value)} value={name} />
+        </div>
+    );
+}
 
 export default ReactHooks;
