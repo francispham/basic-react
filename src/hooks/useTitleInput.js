1ab410d9c4ba3352595b1dcf64274399969a7111
Francis <francispham89@gmail.com>
2020-03-04 16:02:02 -0800

    Cleaned Up Code

--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function useTitleInput(initialValue) {
     const [value, setValue] = useState(initialValue);
