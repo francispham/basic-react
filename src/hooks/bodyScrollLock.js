78b7102e7254209af4084ab00f19d0a614cb30d9
Francis <francispham89@gmail.com>
2020-03-08 16:33:16 -0700

    Added 'Cleanup' Functionality for Hooks

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -2,8 +2,18 @@ import { useLayoutEffect } from 'react';
 
 function useBodyScrollLock() {
     useLayoutEffect(() => {
+        const originalOverflow = window.getComputedStyle(document.body).overflow;
         document.body.style.overflow = 'hidden';
-    });
+
+        return function () {
+            document.body.style.overflow = originalOverflow;
+        }
+        /* Similar: 
+        return function cleanup() {
+            document.body.style.overflow = '';
+        }
+        */
+    }, []); // [] is to ensure useLayoutEffect() to run once!
 };
 
 export { useBodyScrollLock };
